d32ef29a8c4f2b73e1f445b35e42b4744e6f5d07
bztlgx <maohui.zhou@gm.com>
2019-01-11 21:52:02 -0600

    Jan 11, 2019

---
+++ src/main/java/leetcode/Jan_11_2019/LamdaExpression/FunctionalInterFace.java
@@ -0,0 +1,9 @@
+package main.java.leetcode.Jan_01_11_2019.LamdaExpression;
+
+public class FunctionalInterFace
+{
+    public static void  main(String args[]){
+        System.out.println("hello!");
+    }
+
+}
